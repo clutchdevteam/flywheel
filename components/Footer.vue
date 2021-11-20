e393b5d59fd440f083dffd4586b0d1a9e11a4d32
Drew <drewclementsdesign@gmail.com>
2021-11-19 20:41:24 -0500

    Init scaffold setup

---
+++ components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer>Footer</footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="postcss" scoped></style>
---
+++ components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>Header</header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="postcss" scoped></style>
