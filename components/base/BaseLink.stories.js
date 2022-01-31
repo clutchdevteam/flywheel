565b9b8eea17723dbe65345d06f8db71fdfadc7e
Drew <drewclementsdesign@gmail.com>
2022-01-30 22:04:47 -0500

    base link

---
+++ components/base/BaseLink.stories.js
@@ -0,0 +1,30 @@
+export default {
+  title: "Base/Link",
+};
+
+export const Default = (args) => ({
+  props: Object.keys(args),
+  computed: {
+    slotContent() {
+      return this.default;
+    },
+  },
+  template:
+    '<BaseLink v-bind="$props" target="_blank">{{ slotContent }}</BaseLink>',
+});
+
+Default.argTypes = {
+  default: {
+    control: "text",
+  },
+  href: {
+    control: {
+      type: "text",
+    },
+  },
+};
+
+Default.args = {
+  default: "Check out Flywheel",
+  href: "https://github.com/clutchdevteam/flywheel",
+};
---
+++ components/base/BaseLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <a class="cursor-pointer" :href="href" v-bind="$attrs">
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    href: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
